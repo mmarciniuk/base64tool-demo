fca90c7a1d040d045555b565455069fff0a55248
Mariusz <mariusz2048@gmail.com>
2019-07-03 01:28:34 +0200

    Updating JavaScript which clear form.

--- src/main/resources/static/js/clearBase64Form.js
+++ src/main/resources/static/js/clearBase64Form.js
@@ -1,4 +1,20 @@
-function clearForm() {
-    document.getElementById("originalPayload").value = "";
-    document.getElementById("modifiedPayload").value = "";
+function clearForm(elementId) {
+    var form = document.getElementById(elementId);
+
+    if (form.tagName === "FORM") {
+        var elementsList = form.elements;
+
+        for (i = 0; i < elementsList.length; i++) {
+            if (elementsList[i].nodeName === "INPUT" && elementsList[i].type === "text") {
+                elementsList[i].value = "";
+            }
+            if (elementsList[i].nodeName === "TEXTAREA") {
+                elementsList[i].value = "";
+            }
+        }
+
+
+    } else {
+        console.error("Provided element is not a FORM tag");
+    }
 }
